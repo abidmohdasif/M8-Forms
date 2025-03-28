c09c872e9f3c00501536e3d1dc96d98c94b0a523
Abid Asif <amasif@my.waketech.edu>
2025-03-28 10:44:50 -0400

    Im Done Finally

--- validationScript.js
+++ validationScript.js
@@ -1,23 +1,10 @@
-// let inputvalue = document.getElementById("inputField")
-// let button = document.getElementById("button")
-// // button.addEventListener("click", function(){
-// //   if (/^[a-zA-Z0-9]+$/.test(inputvalue.value)){
-// //     inputvalue.setCustomValidity("");
-// //     inputvalue.setCustomValidity("Your form has been submitted.")
-// //   } else{
-// //     inputvalue.setCustomValidity("Error! Your input is not alphanumeric!")
-// //   }
-// // });
+document.getElementById("submit").addEventListener("click", (event) => {
 
-
-
-//   button.addEventListener("submit", function(event){
-//     if (!/^[a-zA-Z0-9]+$/.test(inputvalue.value)){
-//       inputvalue.setCustomValidity("Error! Your input is not alphanumeric!");
-//       inputvalue.reportValidity();
-//       event.preventDefault();
-//     } else {
-//       inputvalue.setCustomValidity("");
-//       alert("Form submitted successfully!");
-//     }
-//   });
+    const inputvalue = document.getElementById("inputField")
+    if (/^[a-zA-Z0-9]+$/.test(inputvalue.value)){
+         inputvalue.setCustomValidity("");
+        alert("Your form has been submitted.");
+    } else{
+         inputvalue.setCustomValidity("Error! Your input is not alphanumeric!")
+        }
+  });
